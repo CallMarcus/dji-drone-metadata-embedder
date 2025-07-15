243d4eff4c102e9f09f2210259f4170fd4e13a23
Marcus W. <marcus.westermark@altal.fi>
2025-07-15 23:38:12 +0300

    Add per-frame GPS embedding utilities

--- dji_metadata_embedder/__init__.py
+++ dji_metadata_embedder/__init__.py
@@ -1,3 +1,8 @@
 from .embedder import DJIMetadataEmbedder
+from .per_frame_embedder import embed_flight_path, extract_frame_locations
 
-__all__ = ["DJIMetadataEmbedder"]
+__all__ = [
+    "DJIMetadataEmbedder",
+    "embed_flight_path",
+    "extract_frame_locations",
+]
