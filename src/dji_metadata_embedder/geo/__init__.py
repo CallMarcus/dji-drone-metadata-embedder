1841b1d078a7e99209119a5a1870735bb50ec987
CallMarcus <westermark@gmail.com>
2026-06-04 20:48:52 +0300

    feat(geo): export public geo API

--- src/dji_metadata_embedder/geo/__init__.py
+++ src/dji_metadata_embedder/geo/__init__.py
@@ -1,1 +1,15 @@
 """Geospatial track model and exporters (GeoJSON, KML)."""
+
+from .geojson import convert_to_geojson, track_to_geojson
+from .kml import convert_to_kml, track_to_kml
+from .track import Track, TrackPoint, build_track
+
+__all__ = [
+    "Track",
+    "TrackPoint",
+    "build_track",
+    "track_to_geojson",
+    "convert_to_geojson",
+    "track_to_kml",
+    "convert_to_kml",
+]
